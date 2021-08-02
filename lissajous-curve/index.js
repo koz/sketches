f4ab5d2d9d04927cb19ca870db5d6ba159743566
Gabriel Kozlowaski <gabriel.kozlowaski@booking.com>
2021-08-02 13:42:07 +0200

    Add prettier

--- lissajous-curve/index.js
+++ lissajous-curve/index.js
@@ -10,16 +10,13 @@ A -> Amplitude in X
 B -> Amplitude in Y
 a/b -> Figure appearance
 */
-const getLissajousCurve = ({
-  amplitudeX,
-  amplitudeY,
-  offsetX,
-  offsetY,
-  a,
-  b,
-  angle = Math.PI / 2,
-  length,
-}) => new Array(length).fill(null).map((_, i) => [offsetX + amplitudeX * Math.sin(a * i * 0.01 + angle), offsetY + amplitudeY * Math.sin(b * i * 0.01)])
+const getLissajousCurve = ({ amplitudeX, amplitudeY, offsetX, offsetY, a, b, angle = Math.PI / 2, length }) =>
+  new Array(length)
+    .fill(null)
+    .map((_, i) => [
+      offsetX + amplitudeX * Math.sin(a * i * 0.01 + angle),
+      offsetY + amplitudeY * Math.sin(b * i * 0.01),
+    ]);
 
 // You can force a specific seed by replacing this with a string value
 const settings = {
@@ -27,33 +24,34 @@ const settings = {
   orientation: 'portrait',
   pixelsPerInch: 300,
   scaleToView: true,
-  units: 'cm'
+  units: 'cm',
 };
 
 const sketch = ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, width, height);
 
-    const lines = new Array(30).fill(null).map((l, index) => {
-      return getLissajousCurve({
-        amplitudeX: width / 2.2,
-        amplitudeY: height / 3,
-        offsetX: width / 2,
-        offsetY: height / 2 + index * 0.2 - 4.3,
-        a: 3,
-        b: 5,
-        angle: 0,
-        length: 650
-      })
-    })
+  const lines = new Array(30).fill(null).map((l, index) => {
+    return getLissajousCurve({
+      amplitudeX: width / 2.2,
+      amplitudeY: height / 3,
+      offsetX: width / 2,
+      offsetY: height / 2 + index * 0.2 - 4.3,
+      a: 3,
+      b: 5,
+      angle: 0,
+      length: 650,
+    });
+  });
 
-    return props => renderPaths(lines, {
-          ...props,
+  return (props) =>
+    renderPaths(lines, {
+      ...props,
       lineWidth: 0.08,
       lineJoin: 'round',
       // Optimize SVG paths for pen plotter use
-      optimize: true
-    })
+      optimize: true,
+    });
 };
 
 canvasSketch(sketch, settings);
--- sin-waves/sin-waves.js
+++ sin-waves/sin-waves.js
@@ -1,7 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
 };
 
 const getSinCurve = ({
@@ -9,41 +9,39 @@ const getSinCurve = ({
   offsetX = 0,
   amplitude,
   period = 10, // wave length
-  distance
+  distance,
 }) => {
-  const points = []
+  const points = [];
   let counter = 0;
   for (i = offsetX; i < distance; i += period) {
-    points.push([i, offsetY + (Math.sin(counter) * amplitude)])
+    points.push([i, offsetY + Math.sin(counter) * amplitude]);
     counter += period / 200;
   }
-  return points
-}
-
+  return points;
+};
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-
-    for (s = 0; s < 1600; s+=800) {
-      for (l = 0; l < 600; l+=10) {
+    for (s = 0; s < 1600; s += 800) {
+      for (l = 0; l < 600; l += 10) {
         const points = getSinCurve({
           offsetY: 330 + l + s,
           amplitude: 300,
           period: 10,
-          distance: width
-        })
+          distance: width,
+        });
 
         context.beginPath();
         context.strokeStyle = 'green';
         points.forEach((p, i) => {
           if (i === 0) {
-            return
+            return;
           }
-          context.lineTo(p[0], p[1])
-        })
+          context.lineTo(p[0], p[1]);
+        });
         context.stroke();
         context.closePath();
       }
